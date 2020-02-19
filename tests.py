0c2fc755a929615534567760ab5faa7a5bdbf56c
Likhitha <akulalikhitha1@gmail.com>
2020-02-19 12:58:14 +0530

    normal chnages

--- tests.py
+++ tests.py
@@ -3,27 +3,16 @@ import unittest
 from pyramid import testing
 
 
-class ViewTests(unittest.TestCase):
+class TutorialViewTests(unittest.TestCase):
     def setUp(self):
         self.config = testing.setUp()
 
     def tearDown(self):
         testing.tearDown()
 
-    def test_my_view(self):
-        from .views import my_view
+    def test_hello_world(self):
+        from tutorial import hello_world
+
         request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], 'scaffolds')
-
-
-class FunctionalTests(unittest.TestCase):
-    def setUp(self):
-        from scaffolds import main
-        app = main({})
-        from webtest import TestApp
-        self.testapp = TestApp(app)
-
-    def test_root(self):
-        res = self.testapp.get('/', status=200)
-        self.assertTrue(b'Pyramid' in res.body)
+        response = hello_world(request)
+        self.assertEqual(response.status_code, 200)
